f240a6d530a0a4736dc44a1b3a73607667e29719
EBOBO MAXWELL OGHENEMAGA <oghenemaga.ebobo@unn.edu.ng>
2021-11-08 11:34:32 +0100

    Starting of React Project with W3s

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello World, welcome to Max First React App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myfirstelement = <h1>Hello React!</h1>
+
+ReactDOM.render(myfirstelement, document.getElementById('root'));
+
+/*
+You are now watching
+the React file 'index.js'
+through our 'Show React' tool.
+*/
